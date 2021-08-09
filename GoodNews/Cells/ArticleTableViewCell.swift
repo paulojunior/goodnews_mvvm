b17c18f393b67836c64d579887594e98c1b6ddc7
Paulo Junior <jr@live.at>
2021-08-09 09:20:27 -0300

    First Commit

--- GoodNews/Cells/ArticleTableViewCell.swift
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- GoodNews/Model/Article.swift
+++ GoodNews/Model/Article.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct ArticleList: Decodable {
+    let articles: [Article]
+}
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
